b4827d203d4bfbf2819560fd6b447540b543783b
Nyi Nyi Htun Lwin <nnhl.mgk@gmail.com>
2018-02-04 22:37:52 +0630

    Encode Type Select and Output Image Height Select Completed.

--- app/src/main/java/projects/nyinyihtunlwin/codescanner/activities/GenerateCodeActivity.java
+++ app/src/main/java/projects/nyinyihtunlwin/codescanner/activities/GenerateCodeActivity.java
@@ -4,12 +4,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatSpinner;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
@@ -21,7 +26,6 @@ import com.journeyapps.barcodescanner.BarcodeEncoder;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import projects.nyinyihtunlwin.codescanner.R;
-import projects.nyinyihtunlwin.codescanner.dialogs.EncodeTypeAndSizeChooserDialog;
 
 /**
  * Created by Dell on 2/2/2018.
@@ -32,14 +36,23 @@ public class GenerateCodeActivity extends BaseActivity implements View.OnClickLi
     @BindView(R.id.btn_generate_barcode)
     Button btnGenerateBarCode;
 
-    @BindView(R.id.btn_generate_qr_code)
-    Button btnGenerateQRCode;
-
     @BindView(R.id.et_input)
     EditText etInput;
 
     @BindView(R.id.iv_result_code)
     ImageView ivResultCode;
+
+    @BindView(R.id.sp_encode)
+    AppCompatSpinner spEncode;
+
+    @BindView(R.id.sp_width)
+    AppCompatSpinner spWidth;
+
+    @BindView(R.id.ll_output_height)
+    LinearLayout llOutputHeight;
+
+    private BarcodeFormat mBarcodeFormat;
+    private int barcodeHeight;
 
     public static Intent newIntent(Context context) {
         Intent intent = new Intent(context, GenerateCodeActivity.class);
@@ -57,7 +70,55 @@ public class GenerateCodeActivity extends BaseActivity implements View.OnClickLi
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         btnGenerateBarCode.setOnClickListener(this);
-        btnGenerateQRCode.setOnClickListener(this);
+
+        spEncode.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                switch (position) {
+                    case 0:
+                        mBarcodeFormat = BarcodeFormat.QR_CODE;
+                        barcodeHeight = 300;
+                        break;
+                    case 1:
+                        mBarcodeFormat = BarcodeFormat.CODE_39;
+                        break;
+                    case 2:
+                        mBarcodeFormat = BarcodeFormat.CODE_93;
+                        break;
+                    case 3:
+                        mBarcodeFormat = BarcodeFormat.CODE_128;
+                        break;
+                    case 4:
+                        mBarcodeFormat = BarcodeFormat.EAN_8;
+                        break;
+                    case 5:
+                        mBarcodeFormat = BarcodeFormat.EAN_13;
+                        break;
+                }
+                if (mBarcodeFormat == BarcodeFormat.QR_CODE) {
+                    llOutputHeight.setVisibility(View.GONE);
+                } else {
+                    llOutputHeight.setVisibility(View.VISIBLE);
+                    barcodeHeight = 50;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        spWidth.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                barcodeHeight = Integer.parseInt(getResources().getStringArray(R.array.output_height)[i]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
     }
 
@@ -76,22 +137,40 @@ public class GenerateCodeActivity extends BaseActivity implements View.OnClickLi
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_generate_barcode:
+
+                // hide soft input
+                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(btnGenerateBarCode.getWindowToken(), 0);
+
                 if (!etInput.getText().toString().equals("")) {
-                    String str = etInput.getText().toString();
-                    if (str.matches("[0-9]+")) {
-                        EncodeTypeAndSizeChooserDialog dialog=new EncodeTypeAndSizeChooserDialog();
-                        dialog.show(getSupportFragmentManager(),"choose");
-                        //  generateBarcode(etInput.getText().toString());
+                    String textData = etInput.getText().toString();
+                    if (mBarcodeFormat == BarcodeFormat.QR_CODE) {
+                        generateBarcode(textData, mBarcodeFormat, barcodeHeight);
                     } else {
-                        Toast.makeText(getApplicationContext(), "Barcode can generate only numbers.", Toast.LENGTH_SHORT).show();
+                        // for barcode
+                        if (textData.matches("[0-9]+")) {
+                            switch (mBarcodeFormat) {
+                                case EAN_8:
+                                    if (textData.length() == 8) {
+                                        generateBarcode(textData, mBarcodeFormat, barcodeHeight);
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "EAN_8 can generate exactly 8 numbers.", Toast.LENGTH_SHORT).show();
+                                    }
+                                    break;
+                                case EAN_13:
+                                    if (textData.length() == 13) {
+                                        generateBarcode(textData, mBarcodeFormat, barcodeHeight);
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "EAN_13 can generate exactly 13 numbers.", Toast.LENGTH_SHORT).show();
+                                    }
+                                    break;
+                                default:
+                                    generateBarcode(textData, mBarcodeFormat, barcodeHeight);
+                            }
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Barcode can generate only numbers.", Toast.LENGTH_SHORT).show();
+                        }
                     }
-                } else {
-                    etInput.setError("Enter text first!");
-                }
-                break;
-            case R.id.btn_generate_qr_code:
-                if (!etInput.getText().toString().equals("")) {
-                    generateQRCode(etInput.getText().toString());
 
                 } else {
                     etInput.setError("Enter text first!");
@@ -100,22 +179,10 @@ public class GenerateCodeActivity extends BaseActivity implements View.OnClickLi
         }
     }
 
-    private void generateBarcode(String data) {
+    private void generateBarcode(String data, BarcodeFormat barcodeFormat, int imageHeight) {
         MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
         try {
-            BitMatrix bitMatrix = multiFormatWriter.encode(data, BarcodeFormat.CODE_39, 600, 300);
-            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
-            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
-            ivResultCode.setImageBitmap(bitmap);
-        } catch (WriterException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void generateQRCode(String data) {
-        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
-        try {
-            BitMatrix bitMatrix = multiFormatWriter.encode(data, BarcodeFormat.QR_CODE, 300, 300);
+            BitMatrix bitMatrix = multiFormatWriter.encode(data, barcodeFormat, 600, imageHeight);
             BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
             Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
             ivResultCode.setImageBitmap(bitmap);
